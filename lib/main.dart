280179051d0a07f840da205b4242434a4b1b0545
tokushigeshuya <narihare1130@gmail.com>
2024-01-19 13:55:47 +0900

    [add]クラスの定義とfirebasetono

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:chat/firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'post.dart';
 
 Future<void> main() async {
   // main 関数でも async が使えます
@@ -47,6 +49,7 @@ class _SignInPageState extends State<SignInPage> {
     await FirebaseAuth.instance.signInWithCredential(credential);
   }
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -60,9 +63,83 @@ class _SignInPageState extends State<SignInPage> {
             await signInWithGoogle();
             // ログインが成功すると FirebaseAuth.instance.currentUser にログイン中のユーザーの情報が入ります
             print(FirebaseAuth.instance.currentUser?.displayName);
+
+            // ログイン成功でchatPageに遷移
+            // 前のページに戻れないようにするにはpushAndRemoveUntilを使う
+            if(mounted){
+              Navigator.of(context).pushAndRemoveUntil(
+                MaterialPageRoute(builder: (context){
+                  return const ChatPage();
+                }), 
+                (route) => false,
+                );
+            }
           },
         ),
       ),
     );
   }
 }
+
+
+// チャットページ
+class ChatPage extends StatefulWidget {
+  const ChatPage({super.key});
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('チャット'),
+      ),
+      body: Center(
+        child: TextFormField(
+          onFieldSubmitted: (text){
+            // user 変数にログイン中のデータを渡す
+            final user = FirebaseAuth.instance.currentUser!;
+            // ログイン中のユーザーのIDが取れる
+            final posterId = user.uid;
+            // googleアカウントの名前が取れる
+            final posterName = user.displayName!;
+            // googleのアイコン
+            final posterImageUrl = user.photoURL!;
+
+            // 先ほど作ったpostseference　からランダムなIDのドキュメントリファレンスを作成する
+            // docの引数を空にするとランダムなIDが採用される
+            final newDocumentReference = postsReference.doc();
+
+            final newPost = Post(
+              text: text,
+              createdAt: Timestamp.now(),
+              posterName: posterName,
+              posterImageUrl: posterImageUrl,
+              posterId: posterId,
+              reference: newDocumentReference,
+            );
+
+            // 先ほど作ったnewDocumentReferenceのset関数を実行するとそのドキュメントデータが保存される
+            // 引数としてPost インスタンスを渡します。
+            // 通常はMapしか受け付けませんが、withConverter を使用したことによりPostインスタンスを受け取れるようになる
+            newDocumentReference.set(newPost);
+          },
+        ),
+      ),
+    );
+  }
+}
+
+// <Post>は変換したい型名を入れる
+final postsReference = FirebaseFirestore.instance.collection('posts').withConverter<Post>(
+  // 第二引数は使用しないので_で不使用であることをわかりやすくする
+  fromFirestore: ((snapshot, _) {
+    return Post.fromFirestore(snapshot);
+  }), 
+  toFirestore: ((value, _){
+    return value.toMap();
+  }),
+  );
